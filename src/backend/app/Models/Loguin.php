fe631344658fa6b3be1166d15e171264a1edbfc6
Fino <46083@fiecalunos.lan>
2026-09-15 16:43:33 -0300

    Criação dos models de Armazenar/buscar/layers/loguin

---
+++ src/backend/app/Models/Loguin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Loguin extends Model
+{
+    protected $fillable = [];
+}
---
+++ src/backend/app/Models/armazenar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class armazenar extends Model
+{
+    protected $fillable = [];
+}
